f9166a897d428a5862199fb920602ad7f24ee01e
diegolenci <lencidiego0@gmail.com>
2025-03-05 11:20:03 +0100

    Add: create folders and files

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './HomePage.css'
+import Navbar from "../../components/Navbar/Navbar";
+
+const HomePage = () => {
+    return(
+        <div className="home">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default HomePage
