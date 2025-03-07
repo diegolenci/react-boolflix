2d6b3c07b344ad07b21e9fe92c36c53ac6962185
diegolenci <lencidiego0@gmail.com>
2025-03-07 16:31:46 +0100

    Add: card.jsx + struttura

--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -1,12 +1,18 @@
-export function ResultsList(){
+import { Card } from "./Card"
+
+export function ResultsList({type, items}){
     return(
         <section>
             <p>
-                risultati
+                risultati: {items.length}
             </p>
             <ul>
                 {
-                    
+                    items && items.length > 0 ? (
+                        items.map( item => <Card key={item.id} data={item} type={type} /> )
+                    ) : (
+                        <p>Nessun risultato</p>
+                    )
                 }
             </ul>
         </section>
