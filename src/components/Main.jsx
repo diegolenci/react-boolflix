9565b41ac3239c1d854f018ce57227bd9165b752
diegolenci <lencidiego0@gmail.com>
2025-03-07 11:37:49 +0100

    Add: stampo lista film e serie tv in pagina

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,36 @@
+import { useGlobalContext } from "../context/GlobalContext"
+
 export function Main(){
+
+    const { movies, tvs } = useGlobalContext()
+
     return(
-        <h1>main</h1>
+        <main>
+            <h2>Films</h2>
+            <ul>
+                {
+                    movies.map( (elem) => {
+                        return(
+                            <li key={elem.id}>
+                                {elem.title} - {elem.original_language} - {elem.original_title} - {Math.ceil(elem.vote_average / 2)}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            
+            <h2>Serie tv</h2>
+            <ul>
+                {
+                    tvs.map( (elem) => {
+                        return(
+                            <li key={elem.id}>
+                                {elem.name}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </main>
     )
 }
