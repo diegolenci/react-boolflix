55f6dece5c36ddedbdb325cd73e3759014035415
diegolenci <lencidiego0@gmail.com>
2025-03-07 14:44:50 +0100

    Add: component resultsList + importazione con i prop

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,31 @@
 import { useGlobalContext } from "../context/GlobalContext"
+import { ResultsList } from "./ResultsList"
 
 export function Main(){
 
     const { movies, tvs } = useGlobalContext()
 
+    //const HandleStars = (vote) => Math.ceil( vote / 2)
+
     return(
         <main>
             <h2>Films</h2>
-            <ul>
+            <ResultsList type="movies" items={movies}/>
+            {/* <ul>
                 {
                     movies.map( (elem) => {
                         return(
                             <li key={elem.id}>
-                                {elem.title} - {elem.original_language} - {elem.original_title} - {Math.ceil(elem.vote_average / 2)}
+                                {elem.title} - {elem.original_language} - {elem.original_title} - {HandleStars( parseInt (elem.vote_average) )}
                             </li>
                         )
                     })
                 }
-            </ul>
+            </ul> */}
             
             <h2>Serie tv</h2>
-            <ul>
+            <ResultsList type="tvs" items={tvs}/>
+            {/* <ul>
                 {
                     tvs.map( (elem) => {
                         return(
@@ -30,7 +35,7 @@ export function Main(){
                         )
                     })
                 }
-            </ul>
+            </ul> */}
         </main>
     )
 }
---
+++ src/components/ResultsList.jsx
@@ -0,0 +1,14 @@
+export function ResultsList(){
+    return(
+        <section>
+            <p>
+                risultati
+            </p>
+            <ul>
+                {
+                    
+                }
+            </ul>
+        </section>
+    )
+}
