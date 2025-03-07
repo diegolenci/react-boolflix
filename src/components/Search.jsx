7ee2e6b1a5e8d09a8cf968e0eb5ff03204572ccd
diegolenci <lencidiego0@gmail.com>
2025-03-07 10:20:37 +0100

    Add: funzione per raccogliere stringa che utente scrive nel form

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
+
 export function Search(){
+
+    const [query, setQuery] = useState('')
+
+
+    const HandleSubmit = (e) => {
+        e.preventDefault()
+        console.log(query)
+    }
+    
     return(
         <>
-            <form action="">
-                <input type="text" />
+            <form onSubmit={e => HandleSubmit(e)}>
+                <input type="text" onChange={ e => setQuery( e.target.value )}/>
                 <button type="submit">Cerca</button>
             </form>
         </>
