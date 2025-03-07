fdf2eef18324848b32f120804dcabb81556db5ac
diegolenci <lencidiego0@gmail.com>
2025-03-07 11:05:52 +0100

    Add: file: .env + import api key e endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { Header } from "./components/Header"
 import { Main } from "./components/Main"
 import { GlobalProvider } from "./context/GlobalContext"
 
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=1bb80835710cfd4c04e384c234391bbc'
-
 
 function App() {
   
--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,15 +1,25 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import axios from "axios";
 
 const GlobalContext = createContext()
 
 const GlobalProvider = ({children}) => {
 
+    //dati .env
+    const apikey = import.meta.env.VITE_API_KEY
+    const endpoint = import.meta.env.VITE_ENDPOINT
+
     const [query, setQuery] = useState('')
+    const [movies, setMovies] = useState([])
 
     const HandleSubmit = (e) => {
         e.preventDefault()
-        console.log(query)
+
+        //'https://api.themoviedb.org/3/discover/movie?api_key=1bb80835710cfd4c04e384c234391bbc'
+        axios.get(`${endpoint}movie?api_key=${apikey}`)
+        .then( res => setMovies(res.data.results) )
+        .catch( err => console.log(err) )
+
     }
 
     const value = {
